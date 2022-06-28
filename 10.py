885908299f5774c5edfadceafefd09513b8309e5
Mahil Reshi <mahilreshi5@gmail.com>
2022-06-28 12:33:58 +0530

    2

--- 10.py
+++ 10.py
@@ -1,10 +1,51 @@
-n = int(input("Enter the number\n"))
-for i in range(n) :
-    fact = n * fact(n-1) 
-    if (n == 1):
-        print("1")
-    elif (n==0) :
-        print("1")    
-    else :    
-      print(fact)
+import random
+
+# stone, paper, scissor
+
+opt =  ["stone", "paper", "scissor"]
+
+comp = random.choice(opt)
+
+print(comp)
+
+stone = "stone"
+paper = "paper"
+scissor = "scissor"
+
+plyr = input("Select your choice stone, paper or scissor\n")
+
+if (plyr == stone and comp  == paper):
+    print("Computer wins! (Paper)")
+  
+elif (plyr == stone and comp == scissor):
+    print("You won! (Scissor)")
+
+elif(plyr == stone and comp == stone):
+    print("$Draw$") 
+
+if (plyr == paper and comp == scissor):
+    print("Computer wins! (Paper)")
+  
+elif (plyr == paper and comp == stone):
+    print("You won! (Scissor)")
+
+elif(plyr == paper and comp == paper):
+    print("$Draw$")
+
+if (plyr == scissor and comp == stone):
+    print("Computer wins! (Paper)")
+  
+elif (plyr == scissor and comp == paper):
+    print("You won! (Scissor)")
+
+elif(plyr == scissor and comp == scissor):
+    print("$Draw$")
     
+
+
+
+ 
+     
+
+
+            
